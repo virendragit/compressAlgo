5263c4951cd88773cb9fdd2b6b17604585671a1a
Virendra Gupta <vgupta@ivycomptech.com>
2020-01-08 11:48:06 +0530

    first commit

---
+++ CompressAlgo.playground/Contents.swift
@@ -0,0 +1,21 @@
+// Problem description
+//
+// characters are repeated and emitting a condensed description of those characters
+//
+// I'm not as interested in the right or wrong answer, I just want to see your thought process.  Talk through what you're thinking.
+// I don't care about what language you use, or that you use correct syntax. It can even be pseudo code.
+//
+// Examples
+// compress("AAAABBAA")    -> "4xAB2xA"
+// compress("CCCCCC")   -> "6xC"
+// compress("ABCD")     -> "ABCD"
+// compress("4236")     -> "4236"
+// compress("BBAACCDD") -> "2xB2xA2xC2xD"
+
+// Implement the compression algorithm described above
+
+
+
+import UIKit
+
+
